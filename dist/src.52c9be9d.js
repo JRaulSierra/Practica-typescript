9ed01b4e38b57777201959994618934575c5d373
Raul Sierra <70240223+JRaulSierra@users.noreply.github.com>
2023-02-07 00:06:18 +0000

    Basic functions

---
+++ dist/src.52c9be9d.js
@@ -0,0 +1,8 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"Lat4":[function(require,module,exports) {
+
+},{}],"yeZa":[function(require,module,exports) {
+"use strict";function n(n,o){return n+o}var o=function(n,o){return n+o};function r(n,o){return n+o}function e(n,o){var r=n+o;console.log(r)}var s={name:"HOla",username:"Hola 2",address:function(){console.log("return void")},address_2:function(){return 1}};console.log(s.address_2()),console.log(n,o,r,e,s);
+},{}],"B6dB":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),require("./style.css"),require("./practica_typescript/2.FunctionsBasics");
+},{"./style.css":"Lat4","./practica_typescript/2.FunctionsBasics":"yeZa"}]},{},["B6dB"], null)
+//# sourceMappingURL=/src.52c9be9d.js.map
